ab875bb3d1fa16191be9f3fb25c95d405aec6c30
cor <cor@pruijs.nl>
2016-03-22 11:04:42 +0100

    Add motor script

---
+++ Assets/Scripts/BikeController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BikeController : MonoBehaviour {
+    
+    public List<AxleInfo> axleInfos;
+    public float maxMotorTorque;
+    public float maxSteeringAngle;
+
+	void FixedUpdate () {
+	    float motorInput = maxMotorTorque * Input.GetAxis("Vertical");
+        float steeringInput = maxSteeringAngle * Input.GetAxis("Horizontal");
+        
+        foreach (AxleInfo axleInfo in axleInfos) {
+            if (axleInfo.steering) {
+                axleInfo.wheel.steerAngle = steeringInput;
+            }
+            
+            if (axleInfo.motor) {
+                axleInfo.wheel.motorTorque = motorInput;
+            }
+        }
+	}
+}
+[System.Serializable]
+public class AxleInfo {
+    public WheelCollider wheel;
+    public bool motor;
+    public bool steering;
+}

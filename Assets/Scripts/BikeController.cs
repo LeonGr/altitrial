60a4d0344aa45e4da29577bea3cb7821ef11a663
cor <cor@pruijs.nl>
2016-03-29 11:07:12 +0200

    Fix wheel rotation bug

--- Assets/Scripts/BikeController.cs
+++ Assets/Scripts/BikeController.cs
@@ -8,24 +8,50 @@ public class BikeController : MonoBehaviour {
     public float maxMotorTorque;
     public float maxSteeringAngle;
 
+
+    // finds the corresponding visual wheel
+    // correctly applies the transform
+    public void ApplyLocalPositionToVisuals(WheelCollider collider) {
+        if (collider.transform.childCount == 0) {
+            return;
+        }
+        
+        Transform visualWheel = collider.transform.GetChild(0);
+    
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+        rotation *= Quaternion.Euler(0, 0, 90);
+        
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
+    }
+      
+
 	void FixedUpdate () {
 	    float motorInput = maxMotorTorque * Input.GetAxis("Vertical");
         float steeringInput = maxSteeringAngle * Input.GetAxis("Horizontal");
         
         foreach (AxleInfo axleInfo in axleInfos) {
             if (axleInfo.steering) {
-                axleInfo.wheel.steerAngle = steeringInput;
+                axleInfo.rightWheel.steerAngle = steeringInput;
+                axleInfo.leftWheel.steerAngle = steeringInput;
             }
             
             if (axleInfo.motor) {
-                axleInfo.wheel.motorTorque = motorInput;
+                axleInfo.rightWheel.motorTorque = motorInput;
+                axleInfo.leftWheel.motorTorque = motorInput;
             }
+        
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
         }
 	}
 }
 [System.Serializable]
 public class AxleInfo {
-    public WheelCollider wheel;
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
     public bool motor;
     public bool steering;
 }

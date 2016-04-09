27beaf1eba7988fb1eddc6cc10cb9168d6ee3690
cor <cor@pruijs.nl>
2016-04-09 23:19:47 +0200

    Add smooth camera follow movement

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,41 +4,34 @@ public class CameraController : MonoBehaviour {
 
 
   
-    public GameObject target;
+    public Transform target;
+    public Transform targetCameraPosition;
  
-    private Vector3 offet;
+    private Vector3 relativeCameraPos;
     
-    public float damp = 0.5f;
-
-   // Use this for initialization
-   void Start () {
-
-        offet = transform.position;
-   }
-   
-
-    void LateUpdate()
-    {
-        Vector3 currentPos = target.transform.position;
-       
-        float currentHeight = this.transform.position.y;
- 
-        //wantedHeight should be the height of the car's position plus some distance
-        float wantedHeight = target.transform.position.y + 10;
-    
-        //lerp from currentHeight to wantedHeight
-        currentHeight = Mathf.Lerp(currentHeight,wantedHeight, damp * Time.deltaTime);
+    public float smooth = 1.5f;
+  
+  
+    void FixedUpdate () {
         
-        //add a distance between the car's position and the camera's desired position, in the x coordinate because
-        //of how the x/z axes on the cars are reversed
-        Vector3 wantedPos = new Vector3(currentPos.x - 10, currentHeight, currentPos.z);
-                
-        //finally, set the camera's position to its desired position
-        this.transform.position = wantedPos;
+        transform.position = Vector3.Lerp(transform.position, targetCameraPosition.position, smooth * Time.deltaTime);
         
-        this.transform.LookAt(target.transform);
-      
+        SmoothLookAt();
     }
+    
+    
+    void SmoothLookAt ()
+    {
+        // Create a vector from the camera towards the player.
+        Vector3 relPlayerPosition = target.position - transform.position;
+        
+        // Create a rotation based on the relative position of the player being the forward vector.
+        Quaternion lookAtRotation = Quaternion.LookRotation(relPlayerPosition);
+        
+        // Lerp the camera's rotation between it's current rotation and the rotation that looks at the player.
+        transform.rotation = Quaternion.Lerp(transform.rotation, lookAtRotation, smooth * Time.deltaTime);
+    }
+
 
 
 }

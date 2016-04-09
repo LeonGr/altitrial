817e626c61d688da298eae76d178cfe127b32f6d
cor <cor@pruijs.nl>
2016-04-10 01:48:15 +0200

    Add basic TowerGenerator

---
+++ Assets/Scripts/TowerGenerator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerGenerator : MonoBehaviour {
+
+    public GameObject towerSegment;
+    
+	// Use this for initialization
+	void Start () {
+        AddSegment();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    void AddSegment() {
+        
+        // Objects get imported with a 90 degrees angle for some reason... this is a simple workaround
+        Quaternion newRotation = transform.rotation;
+        newRotation *= Quaternion.Euler(90, 0, 0);
+        Instantiate(towerSegment, transform.position, newRotation);
+    }
+}

9e3ade94de262d8b33dfa37c054d2e384629644c
cor <cor@pruijs.nl>
2016-04-11 23:57:35 +0200

    Add windows

---
+++ Assets/Scripts/WindowController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindowController : MonoBehaviour {
+
+    public GameObject brokenWindow;
+    public float minimumVelocityForBreaking;
+
+    void OnCollisionEnter(Collision collision) {
+        Debug.Log("new collision");
+        if (collision.relativeVelocity.magnitude > minimumVelocityForBreaking) {
+            Destroy(gameObject);
+            Instantiate(brokenWindow, transform.position, transform.rotation * Quaternion.Euler(-90f, 0, 0));
+        }
+    }
+}
